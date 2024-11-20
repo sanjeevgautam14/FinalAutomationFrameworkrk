c436be02f0305caeb62bf9ccd1b2b49cac43de41
hp <hp@DESKTOP-M8GBR1P>
2024-11-20 14:02:05 +0530

    PollSCM commit

---
+++ FinalAutomationFramework/src/test/java/pollSCM/PollSCM_Test.java
@@ -0,0 +1,15 @@
+package pollSCM;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class PollSCM_Test {
+	
+	@Test
+	public void pollSCM() {
+		Reporter.log("pollSCM class executed");
+		
+	}
+}
